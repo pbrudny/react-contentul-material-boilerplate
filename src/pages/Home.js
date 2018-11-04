e73332f1b02018c31f95d1aa1c6c98da1962f406
pbrudny <pbrudny@gmail.com>
2018-11-04 13:50:01 +0000

    Update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
   render() {
     return(<>
         <h1>Hello :)</h1>
-        <h2>You can try different layouts from Material UI</h2>
+        <h2>Before development try different layouts from Material UI</h2>
         <h3>* navbar will be at the bottom</h3>
       </>
     )
